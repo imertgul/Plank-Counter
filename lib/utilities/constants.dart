2ef7deb7b2be63f5d5cf552387149d37f100b3b1
İbrahim Mert Gül <ibrahimmertgul@gmail.com>
2020-05-18 23:00:32 +0300

    New color

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -29,10 +29,13 @@ class Renkler {
   static const Color pLight = Color(0xff62727b);
   static const Color pDark = Color(0xff102027);
   static const Color textOnP = Colors.white;
-  static const Color secondary = Color(0xff00838f);
-  static const Color sLight = Color(0xff4fb3bf);
-  static const Color sDark = Color(0xff005662);
+  static const Color secondary = Color(0xff79538E);
+  static const Color sLight = Color(0xffAF91BB);
+  static const Color sDark = Color(0xff533266);
   static const Color textOnS = Colors.white;
+  // static const Color secondary = Color(0xff00838f);
+  // static const Color sLight = Color(0xff4fb3bf);
+  // static const Color sDark = Color(0xff005662);
 }
 
 String todayToString() {
